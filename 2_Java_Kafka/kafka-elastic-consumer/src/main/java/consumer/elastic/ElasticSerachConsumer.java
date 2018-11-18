03c441e6a959609ab1175a9d865ee4d34990baf7
Avinash Tiwari <qwe123kids@gmail.com>
2018-11-18 14:28:43 -0700

    no message

--- 2_Java_Kafka/kafka-elastic-consumer/src/main/java/consumer/elastic/ElasticSerachConsumer.java
+++ 2_Java_Kafka/kafka-elastic-consumer/src/main/java/consumer/elastic/ElasticSerachConsumer.java
@@ -12,6 +12,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -64,27 +66,54 @@ public class ElasticSerachConsumer {
 
         while(true){
             ConsumerRecords<String,String> records =  consumer.poll(Duration.ofMillis(100));
+            Integer recordCount = records.count();
+            loggger.info("Recived " + recordCount + " records" );
+
+            BulkRequest bulkRequest = new BulkRequest();
 
             for(ConsumerRecord<String,String>  record: records){
 
-              //  String id = record.topic()  + "_" + record.topic() + "_" + record.offset();
-                String id = extractIdFromTweet(record.value());
+               String id = "";
+                try {
+                     id = extractIdFromTweet(record.value());
+                }catch(Exception ex){
+                    id = record.topic()  + "_" + record.topic() + "_" + record.offset();
+                }
+
+
                  IndexRequest indexRequest = new IndexRequest("twitter","tweets",
                          id)
                         .source(record.value(), XContentType.JSON);
 
-                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                bulkRequest.add(indexRequest);
+              //  IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
                 //String id = indexResponse.getId();
-                loggger.info(indexResponse.getId());
+               // loggger.info(indexResponse.getId());
 
+              //  try {
+                //    Thread.sleep(10);
+                //} catch (InterruptedException e) {
+                   // e.printStackTrace();
+                //}
+
+
+            }
+
+            if(recordCount > 0) {
+
+                BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+
+                loggger.info("Commiting offset..");
+                consumer.commitAsync();
+                loggger.info("Offset had been  cmmited..");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
-
             }
+
+
         }
 
 
@@ -111,6 +140,9 @@ public class ElasticSerachConsumer {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"100");
+
 
         KafkaConsumer<String,String> consumer = new KafkaConsumer<String,String>(properties);
         consumer.subscribe(Arrays.asList(topic));
